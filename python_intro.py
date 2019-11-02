046ea682ae9134865948bca9898c928840bdd58e
Your Name <you@example.com>
2019-11-02 14:18:13 -0500

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,4 @@
+print('Hello, Django girls!')
+name = 'Paige'
+myFavoriteThings = {'snacks': ['apples', 'popcorn cakes', 'pudding with sprinkles'], 'healthy_food': "taco salad", 'vacation_spot': "Rocky Mountains", 'animals': ['cats', 'raccoons', 'owls']}
+print('My name is '+ name +"!")
